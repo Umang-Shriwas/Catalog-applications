f1f8b83a4bf6756445fd87ad2c1c46d2206bb7fe
Umang-Shriwas <umangshriwas17@gmail.com>
2023-08-26 16:30:22 +0530

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Umang";
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Catalog App")),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Catalog APP by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
