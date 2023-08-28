3859d76f5ae055da24b68e8ba3e0d6633b0b6368
Umang-Shriwas <umangshriwas17@gmail.com>
2023-08-28 12:55:58 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tutorials/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tutorials/Pages/home_page.dart';
+
+import 'Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home: HomePage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
